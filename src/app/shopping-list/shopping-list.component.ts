bc43d5a6ed79a74f49e6268e41a0a69b8539fc76
Vladimir Sedlar <vladimir.sedlar@gauss.hr>
2017-05-01 12:50:45 +0200

    added shopping list component

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-shopping-list',
+  templateUrl: './shopping-list.component.html'
+})
+export class ShoppingListComponent {
+
+}
