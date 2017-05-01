a75073751701424315fb6ddaa0113b5e5f1a1ed4
Vladimir Sedlar <vladimir.sedlar@gauss.hr>
2017-05-01 12:22:11 +0200

    connected the list component with the details compnent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import { Recipe } from '../recipe';
 import { RecipeItemComponent } from './recipe-item.component';
 
@@ -8,7 +8,7 @@ import { RecipeItemComponent } from './recipe-item.component';
 })
 export class RecipeListComponent {
     recipes: Recipe[] = [];
-    recipeSelected = new EventEmitter();
+    @Output() recipeSelected = new EventEmitter();
     recipe = new Recipe('Dummy', 'Dummy', 'https://coolinarika-cdn.azureedge.net/image/' +
         'jogurt-sa-hrskavim-sjemenkama-6fe070031f77f926d0212739da2d20fb_header.jpg?v=14');
 
